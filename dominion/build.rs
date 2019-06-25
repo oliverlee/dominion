4edabb9989605ac444cf82e695444e59d1730327
Oliver Lee <oliverzlee@gmail.com>
2019-06-25 21:31:30 +0200

    Generate CardKind for base cards
    Add file base_cards.json with definitions for Copper, Silver, Gold,
    Estate, Duchy, Province, and Curse. Card types for base cards are added
    in the build script as type indices are defined by dominion.json.

--- dominion/build.rs
+++ dominion/build.rs
@@ -11,7 +11,11 @@ use std::process::Command;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut missing_no: usize = 0;
 
-    let Scrape { sets, types, cards } = serde_json::from_reader(BufReader::new(
+    let Scrape {
+        sets,
+        types,
+        mut cards,
+    } = serde_json::from_reader(BufReader::new(
         std::fs::File::open("../dominion.json").unwrap(),
     ))?;
 
@@ -22,19 +26,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let non_ident_regex = Regex::new(r"[^\w\d]+").unwrap();
 
-    let base_card_indices = sets.iter().flat_map(|set| {
-        if &set.name == "Dominion 2nd Edition" {
-            set.card_indices.iter()
-        } else {
-            [].iter()
-        }
+    let baseset_card_indices: Vec<_> = sets
+        .iter()
+        .flat_map(|set| {
+            if &set.name == "Dominion 2nd Edition" {
+                set.card_indices.iter()
+            } else {
+                [].iter()
+            }
+        })
         .copied()
-    });
+        .collect();
 
-    let extended_cards: Vec<CardExt> = base_card_indices
-        .map(|index| {
-            let card = &cards[index as usize];
+    // Verify that the range is contiguous.
+    for (&a, &b) in baseset_card_indices.iter().zip(baseset_card_indices.iter().skip(1)) {
+        assert_eq!(a + 1, b, "card indices are not contiguous");
+    }
 
+    let mut extended_cards: Vec<_> = cards
+        .drain(std::ops::Range {
+            start: baseset_card_indices.first().unwrap(),
+            end: baseset_card_indices.last().unwrap(),
+        })
+        .map(|card| {
             let ident = if card.name.is_empty() {
                 missing_no += 1;
                 format!("MissingNo{}", missing_no)
@@ -45,6 +59,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             CardExt { card, ident }
         })
         .collect();
+
+    extended_cards.append(&mut base_cards(&types));
 
     let declaration_lines = extended_cards
         .iter()
@@ -57,13 +73,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         quote! { CardKind::#ident => #name }
     });
 
-    let is_type_methods = ["Action", "Reaction", "Attack", "Victory", "Treasure"]
-        .iter()
-        .map(|s| {
-            let method = is_type_method(&extended_cards, &types, s);
+    let is_type_methods = CARD_TYPES.iter().map(|s| {
+        let method = is_type_method(&extended_cards, &types, s);
 
-            quote! { #method }
-        });
+        quote! { #method }
+    });
 
     let tokens = quote! {
         #[derive(Debug)]
@@ -83,14 +97,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     };
 
-    // TODO: Base cards are not included
-    // Copper
-    // Silver
-    // Gold
-    // Estate
-    // Duchy
-    // Province
-    // Curse
     file.write_all(tokens.to_string().as_bytes())?;
 
     // Don't forget to flush!
@@ -104,8 +110,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct CardExt<'a> {
-    card: &'a Card,
+const CARD_TYPES: &[&'static str] = &["Action", "Reaction", "Attack", "Victory", "Treasure"];
+
+struct CardExt {
+    card: Card,
     ident: String,
 }
 
@@ -147,4 +155,38 @@ fn is_type_method(cards: &Vec<CardExt>, types: &Vec<String>, type_name: &str) ->
             }
         }
     }
+}
+
+fn base_cards(types: &Vec<String>) -> Vec<CardExt> {
+    let Scrape {
+        sets: _,
+        types: _,
+        cards: mut base_cards,
+    } = serde_json::from_reader(BufReader::new(
+        std::fs::File::open("../base_cards.json").unwrap(),
+    ))
+    .unwrap();
+
+    // Type indices are not set for base cards as they are stored in a separate
+    // file from the rest of the cards.
+    let treasure_index =types.iter().position(|s| s == "Treasure").unwrap();
+    let victory_index = types.iter().position(|s| s == "Victory").unwrap();
+
+    for card in &mut base_cards {
+        let type_index = match card.name.as_ref() {
+            "Copper" | "Silver" | "Gold" => treasure_index,
+            _ => victory_index,
+        };
+
+        card.type_indices.push(type_index);
+    }
+
+    base_cards
+        .into_iter()
+        .map(|card| {
+            let ident = card.name.clone();
+
+            CardExt { card, ident }
+        })
+        .collect()
 }
