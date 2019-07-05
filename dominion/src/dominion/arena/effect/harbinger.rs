ee9453dbf2723c0e848f15a2aa5428e6231fb9f2
Oliver Lee <oliverzlee@gmail.com>
2019-07-05 23:16:36 +0200

    Add unit tests for effect::harbinger

--- dominion/src/dominion/arena/effect/harbinger.rs
+++ dominion/src/dominion/arena/effect/harbinger.rs
@@ -44,3 +44,66 @@ fn func(arena: &mut Arena, player_id: usize, cards: &[CardKind]) -> Result<Outco
 
     Ok(Outcome::None)
 }
+
+#[cfg(test)]
+mod test {
+    use super::super::test_util;
+    use super::*;
+    use crate::dominion::types::Error;
+    use crate::dominion::CardKind;
+
+    #[test]
+    fn empty_discard_pile() {
+        let mut arena = test_util::setup_arena();
+        let player_id = arena.current_player_id;
+
+        let cards = [];
+
+        assert!(arena.current_player().discard_pile.is_empty());
+        assert_eq!(func(&mut arena, player_id, &cards), Ok(Outcome::None));
+    }
+
+    #[test]
+    fn choose_not_to_move_card() {
+        let mut arena = test_util::setup_arena();
+        let player_id = arena.current_player_id;
+
+        let cards = [];
+
+        arena.current_player_mut().discard_pile.push(CardKind::Gold);
+
+        assert_eq!(func(&mut arena, player_id, &cards), Ok(Outcome::None));
+    }
+
+    #[test]
+    fn choose_to_move_card() {
+        let mut arena = test_util::setup_arena();
+        let player_id = arena.current_player_id;
+
+        let cards = [CardKind::Gold];
+
+        arena.current_player_mut().discard_pile.push(cards[0]);
+
+        assert_ne!(arena.current_player().draw_pile.last().unwrap(), &cards[0]);
+        assert_eq!(func(&mut arena, player_id, &cards), Ok(Outcome::None));
+        assert_eq!(arena.current_player().draw_pile.last().unwrap(), &cards[0]);
+        assert!(arena.current_player().discard_pile.is_empty());
+    }
+
+    #[test]
+    fn choose_card_not_in_discard_pile() {
+        let mut arena = test_util::setup_arena();
+        let player_id = arena.current_player_id;
+
+        let cards = [CardKind::Silver];
+
+        arena.current_player_mut().discard_pile.push(CardKind::Gold);
+
+        assert!(!arena.current_player().discard_pile.contains(&cards[0]));
+        assert_eq!(
+            func(&mut arena, player_id, &cards),
+            Err(Error::UnresolvedActionEffect(EFFECT.description()))
+        );
+        assert_eq!(arena.current_player().discard_pile.len(), 1);
+    }
+}
